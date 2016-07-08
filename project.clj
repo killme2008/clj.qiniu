65ddb9124919538ac80910900afd1e8a73907265
dennis zhuang <killme2008@gmail.com>
2016-07-08 10:38:27 +0800

    (feat) Bump clojure and clj-http

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/killme2008/clj.qiniu"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "0.9.2"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "0.7.8"]
                  [com.qiniu/sdk "6.1.0"]])
